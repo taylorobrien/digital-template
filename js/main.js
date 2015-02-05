a21d84975355e214077e05b506198cdfdaf351d9
taylorobrien <tobrie10@gmu.edu>
2015-02-04 22:40:15 -0500

    Updated

--- js/main.js
+++ js/main.js
@@ -19,10 +19,13 @@ window.onload = function() {
         // Load an image and call it 'logo'.
         game.load.image( 'logo', 'assets/phaser.png' );
 		game.load.image('background','assets/graveyard.jpg');
-		game.load.image('guy','assets/purpleguy.png');
+		game.load.image('guy','assets/purpleguy2.png');
 		game.load.image('girl','assets/purplegirl.jpg');
 		game.load.image('rose','assets/rose.png');
 		game.load.audio('song',['assets/BlackedOutLove.mp3','assets/BlackedOutLove.ogg']);
+		game.load.image('potion','assets/potion.png');
+		game.load.script('filterX', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/BlurX.js');
+    	game.load.script('filterY', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/BlurY.js');
     }
     
     var player;
@@ -30,6 +33,12 @@ window.onload = function() {
 	var roses;
 	var cursors;
 	var music;
+	var score=0;
+	var scoretext;
+	var youwin;
+	var s;
+	var potions;
+	var pot;
     
     function create() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -45,7 +54,7 @@ window.onload = function() {
 		//music.autoplay = true;
 		music.play();
 		player = game.add.sprite( game.world.centerX, 500, 'guy' );
-		player.scale.set(.35 , .35 );
+		player.scale.set(1 , 1 );
 		
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
@@ -62,29 +71,51 @@ window.onload = function() {
 		roses.scale.set(.2,.2);
 		roses.enableBody = true;
 		
+		potions = game.add.group();
+		//potions.scale.set(1,1);
+		potions.enableBody = true;
+		
 		//roses.createMultiple(250, 'rose', 0, false);
 		        
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Falling Roses", style );
+		var style2 = { font: "30px Verdana", fill: "#9999ff", align: "center" };
+		youwin = game.add.text( 100, game.world.centerX, "", style2 );
+        //youwin.anchor.setTo( 0.5, 0.0 );
         text.anchor.setTo( 0.5, 0.0 );
-		game.physics.arcade.gravity.y=160;
+		scoretext = game.add.text(650, 15, "Score: 0", style);
+		text.anchor.setTo(.5,0.0);
+		game.physics.arcade.gravity.y=1500;
 		player.body.gravity.y = 1000000;
+	
 		cursors = game.input.keyboard.createCursorKeys();
-		game.time.events.loop(1, falling, this);
+		//game.time.events.loop(1, falling, this);
+		game.time.events.repeat(Phaser.Timer.SECOND * 1, 10000, falling, this);
+		game.time.events.repeat (Phaser.Timer.SECOND * 4, 10000, makepotion, this);
+		//falling();
     }
+	
+	function makepotion(){
+		pot = potions.create(game.world.randomX, -0, 'potion');
+		pot.body.gravity.y = 10;
+		s.name = 'potion' + pot;
+		pot.body.gravity.x = (Math.random()*(500));
+	}
 	
 	function falling() {
 
     for (var i = 0; i < 1; i++)
     {
-        var s = roses.create(game.world.randomX, game.world.randomY, 'rose');
+        s = roses.create(game.world.randomX+100, -300, 'rose');
         s.name = 'rose' + s;
+		//s.rotation = game.physics.arcade.accelerateToPointer( s, this.game.input.activePointer, 500, 500, 500 );
+
         //body.collideWorldBounds = true;
-        s.body.bounce.setTo(1800, 1600);
+       // s.body.bounce.setTo(1800, 1600);
         //s.body.velocity.setTo(10);
-		s.body.gravity.x = (10 + Math.random() * 500);
+		s.body.gravity.x = (Math.random()*(1500));
     }
 
     /*//var fallrose = roses.getFirstExists(false);
@@ -103,6 +134,40 @@ window.onload = function() {
 
 }
 
+	function playergetsrose(){
+		//_roses.kill();
+	
+		score++;
+		//alert(score);
+		scoretext.text = "Score:" + score;	
+		
+		
+	}
+	
+	function blurimage(){
+		
+		var blurX = game.add.filter('BlurX');
+		var blurY = game.add.filter('BlurY');
+		
+		bg.filters = [blurX,blurY];
+		player.filters = [blurX, blurY];
+		roses.filters = [blurX, blurY];
+	}
+	
+	function playergetspotion(){
+		//_roses.kill();
+	
+		score=score-80;
+		//alert(score);
+		scoretext.text = "Score:" + score;	
+		
+		//blurimage();
+		//game.time.events.add(Phaser.Timer.SECOND * 2, blurimage(),this);
+		//blurimage = false;
+		
+		
+	}
+
     
     function update() {
         // Accelerate the 'logo' sprite towards the cursor,
@@ -120,7 +185,14 @@ window.onload = function() {
     {
         player.body.velocity.x = 200;
     }
-
+	if (score == 1000){
+		youwin.text = "Your Memory Has Been Restored! You Win!";
+		//paused();
+		
+	}
+	
+	game.physics.arcade.overlap(potions, player, playergetspotion, null, this);
+	game.physics.arcade.overlap(roses, player, playergetsrose, null, this);
         //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 };
@@ -132,3 +204,4 @@ window.onload = function() {
 //http://premiumpsd.com/wp-content/uploads/2010/11/cartoon-doll-design_full.jpg
 //http://pngimg.com/upload/rose_PNG649.png
 //http://www.newgrounds.com/audio/listen/560586
+//http://fc06.deviantart.net/fs71/f/2011/321/9/f/unidentified_potion__level_1___for_sale_by_adorabless-d4gi9l3.png
